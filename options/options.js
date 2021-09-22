ea69e77387e496f41715bddc5610da2a4d211ac4
Crowdbotics <team@crowdbotics.com>
2021-09-22 17:46:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "restless_king_30752Identifier",
+  url: "https://restless_king_30752Identifier.botics.co",
+  api: "https://restless_king_30752Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
